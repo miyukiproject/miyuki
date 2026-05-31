80f94bf1dcaaf8117b7465fd9ddfb1fe049c4cd7
noiseArch <mativalto@gmail.com>
2026-05-30 21:21:22 -0300

    feat: agrego ErrorBoundary para debugging

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,7 @@ module.exports = {
       colors: {
         "mumuki-skyblue": "#3498DB",
         "mumuki-rose": "#ff5b81",
+        "mumuki-teal": "#0B465D",
         "mumuki-rose-darken": "#d94d6e"
       }
     },
